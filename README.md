a60507cb41a859e71d2b7b0c17f28ad3faf5c187
Qihua <zhongqihua@gmail.com>
2023-01-30 01:03:19 -0500

    Update formulation and README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Solve a beer game (Multi-echelon inventory management problem with four facilities) with perfect information using Gurobi.
+
+
+# Introduction
+MIP benchmark for a beer game (Multi-echelon inventory management problem with four facilities) with perfect information using Gurobi.
+
+## Run
+```shell
+$ git clone -b dev-single-agent git@github.com:qihuazhong/multi-echelon-drl.git 
+```
+
+
+## MIP formulation
+$$
+\begin{aligned}
+& \min \sum_{k\in K}\sum_{j\in J}(C_{b}^{j}B_{k}^{j} + C_{h}^{j}I_{k}^{j})\\ \\
+I_{k-1}^{1}+E_{k-LS^{1}}^{2}-B_{k-1}^{1} &=d_{k}+I_{k}^{1}-B_{k}^{1},\forall k\in K\\
+I_{k-1}^{j}+E_{k-LS^{j}}^{j+1} &=E_{k}^{j}+I_{k}^{j},\forall k\in K,j\in J/\{1\}\\
+\sum_{t=1}^{k}E_{t}^{j} &=\sum_{t=1}^{k}P_{t-LI^{j-1}}^{j-1}-B_{k}^{j}-B_{0}^{j},\forall k\in K,j\in J/\{1\}\\
+B_{k}^{j},P_{k}^{j},I_{k}^{1} &\geq0,\forall j\in J,k\in K\\
+E_{k}^{j} & \geq0,\forall j\in J/\{1\},k\in K
+\end{aligned}
+$$
+
+$$
--- main.py
+++ main.py
@@ -4,72 +4,56 @@ import gurobipy as gp
 from gurobipy import GRB
 from plot_util import plot_gurobi_variables
 
+from tqdm.notebook import tqdm
+import os
+import sys
 
-# class BeerGameGurobi:
-#
-#     def __init__(self, demands=None, init_orders=None, init_backlogs=None, init_inventory=None):
-#         self.J = 4  # num of facilities
-#         self.K = 100  # num of periods
-#
-#         self.L_info = [2, 2, 2, 1]  # info lead times
-#         self.L_ship = [2, 2, 2, 2]  # shipment lead times
-#
-#         self.c_h = [1.0, 0.75, 0.5, 0.25]
-#         self.c_b = [1.0, 0.0, 0.0, 10.0]
-#
-#         self.model = self.init_model_and_variables(demands, init_orders, init_backlogs, init_inventory)
-#
-#     def init_model_and_variables(self, demands, init_orders, init_backlogs, init_inventory):
-#         if demands is None:
-#             demands = np.round(np.maximum(10 + 2 * np.random.randn(self.K), 0)).astype(int)
-#
-#         if init_orders is None:
-#             init_orders = [[4] * 4] * 4
-#
-#         if init_inventory is None:
-#             init_inventory = [8] * 4
-#
-#         if init_backlogs is None:
-#             init_backlogs = [0] * 4
-#
-#         model = gp.Model('beer game')
-#
-#         key_facilities_period = list(product(range(J), range(-1, K)))
-#         key_facilities_period_with_init = list(product(range(J), range(-4, K)))
-#
-#         # define decisions variables
-#         self._orders = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='orders')
-#         self._inventory = model.addVars(key_facilities_period, vtype=GRB.CONTINUOUS, name='inventory')
-#         self._backlogs = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='backlogs')
-#         self._shipments = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='shipments')
-#
-#         model.addConstrs(
-#             (self._orders[j, k] == init_orders[j][k + 4] for j in range(J) for k in range(-4, 0)), name='init_orders')
-#
-#         model.addConstrs(
-#             (self._inventory[j, -1] == init_inventory[j] for j in range(J)), name='init_inv')
-#
-#         model.addConstrs(
-#             (self._backlogs[j, -1] == init_backlogs[j] for j in range(J)), name='init_backlogs')
-#
-#         return model
+sys.path.insert(0, os.path.abspath('./multi-echelon-drl/'))
+
+from register_envs import register_envs
+register_envs()
 
 
-def solve_by_gurobi(demands=None, init_orders=None, init_backlogs=None, init_inventory=None):
+def define_cont_variables(model, J: int, K: int):
+    key_facilities_period = list(product(range(J), range(-1, K)))
+    key_facilities_period_with_init = list(product(range(J), range(-4, K)))
+
+    orders = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='orders')
+    _inventory = model.addVars(key_facilities_period, vtype=GRB.CONTINUOUS, name='inventory')
+    _backlogs = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='backlogs')
+    _shipments = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='shipments')
+
+    return orders, _inventory, _backlogs, _shipments
+
+
+def define_bin_variables(model, J: int, K: int):
+    key_facilities_period = list(product(range(J), range(-1, K)))
+    key_facilities_period_with_init = list(product(range(J), range(-4, K)))
+
+    _x = model.addVars(key_facilities_period, vtype=GRB.BINARY, name='x')
+    _y = model.addVars(key_facilities_period_with_init, vtype=GRB.BINARY, name='y')
+
+    return _x, _y
+
+
+def solve_formulation(demands=None, init_orders=None, init_backlogs=None, init_inventory=None, init_shipments=None):
     J = 4  # num of facilities
-    K = 100  # num of periods
+    K = len(demands) # number of periods
 
     L_info = [2, 2, 2, 1]  # info lead times
     L_ship = [2, 2, 2, 2]  # shipment lead times
 
     c_h = [1.0, 0.75, 0.5, 0.25]
-    c_b = [10.0, 0.0, 0.0, 0.0]
+    c_b = [10.0, 0.00000001, 0.00000001, 0.00000001]  # a small positive value to prevent excessive ordering
+
+    #     c_h = [0.5, 0.5, 0.5, 0.5]
+    #     c_b = [1.0, 1.0, 1.0, 1.0]
 
     if demands is None:
         demands = np.round(np.maximum(10 + 2 * np.random.randn(K), 0)).astype(int)
 
     if init_orders is None:
-        init_orders = [[4]*4]*4
+        init_orders = [[4] * 4] * 4
 
     if init_inventory is None:
         init_inventory = [8] * 4
@@ -79,18 +63,10 @@ def solve_by_gurobi(demands=None, init_orders=None, init_backlogs=None, init_inv
 
     model = gp.Model('beer game')
 
-    key_facilities_period = list(product(range(J), range(-1, K)))
-    key_facilities_period_with_init = list(product(range(J), range(-4, K)))
-
     # define decisions variables
-    _orders = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='orders')
-    _inventory = model.addVars(key_facilities_period, vtype=GRB.CONTINUOUS, name='inventory')
-    _backlogs = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='backlogs')
-    _shipments = model.addVars(key_facilities_period_with_init, vtype=GRB.CONTINUOUS, name='shipments')
+    _orders, _inventory, _backlogs, _shipments = define_cont_variables(model, J, K)
 
-    # Constraints
-
-    # initial conditions
+    # Fix initial conditions
     model.addConstrs(
         (_orders[j, k] == init_orders[j][k + 4] for j in range(J) for k in range(-4, 0)), name='init_orders')
 
@@ -100,91 +76,60 @@ def solve_by_gurobi(demands=None, init_orders=None, init_backlogs=None, init_inv
     model.addConstrs(
         (_backlogs[j, -1] == init_backlogs[j] for j in range(J)), name='init_backlogs')
 
+    model.addConstrs(
+        (_shipments[j, k - 2] == init_shipments[j - 1][k] for j in range(1, J) for k in range(0, 2)),
+        name='init_shipments')
+
     # inventory and backlog
     # - Retailer
     model.addConstrs(
-        (_inventory[j, k - 1] + _backlogs[j, k] + _shipments[j+1, k - L_ship[j]] ==
-         demands[k] + _inventory[j, k] + _backlogs[j, k - 1]
-         for k in range(K) for j in [0]), name='inv_transition_retailer')
+        (_inventory[0, k - 1] - _backlogs[0, k - 1] + _shipments[1, k - L_ship[0]] ==
+         demands[k] + _inventory[0, k] - _backlogs[0, k]
+         for k in range(K)), name='inv_transition_retailer')
 
     # - Inner nodes
     model.addConstrs(
-        (_inventory[j, k - 1] + _backlogs[j, k] + _shipments[j + 1, k - L_ship[j]] ==
-         _orders[j - 1, k - L_info[j - 1]] + _inventory[j, k] + _backlogs[j, k - 1]
-         for k in range(K) for j in range(1, J - 1)), name='inv_transition_suppliers')
+        (_inventory[j, k - 1] + _shipments[j + 1, k - L_ship[j]] ==
+         _shipments[j, k] + _inventory[j, k]
+         for k in range(K) for j in range(1, J - 1)), name='inv_transition_inner_nodes')
 
     # - Upmost Supplier
     model.addConstrs(
-        (_inventory[j, k - 1] + _backlogs[j, k] + _orders[j, k - (L_ship[j] + L_info[j])] ==
-         _orders[j - 1, k - L_info[j - 1]] + _inventory[j, k] + _backlogs[j, k - 1]
-         for k in range(K) for j in [J-1]), name='inv_transition_upmost_supplier')
+        (_inventory[j, k - 1] + _orders[j, k - (L_ship[j] + L_info[j])] ==
+         _shipments[j, k] + _inventory[j, k]
+         for k in range(K) for j in [J - 1]), name='inv_transition_upmost_supplier')
 
-    # shipments: must be less or equal than inventory from last period plus newly received
-    # - Upmost Supplier
+    # backlog
     model.addConstrs(
-        (_shipments[j, k] <= _inventory[j, k - 1] + _orders[j, k - (L_ship[j] + L_info[j])]
-         for k in range(K) for j in [J-1]), name='shipments_constraints_upmost_supplier'
-    )
-
-    # - Inner nodes and the retailer
-    model.addConstrs(
-        (_shipments[j, k] <= _inventory[j, k - 1] + _shipments[j + 1, k - L_ship[j]]
-         for k in range(K) for j in range(0, J - 1)), name='shipments_constraints'
-    )
-
-    # shipments: must be less than the accumulated backlogs and the new demand (Retailer Only)
-    # - Retailer
-    model.addConstrs(
-        (_shipments[j, k] <= _backlogs[j, k - 1] + demands[k]
-         for k in range(K) for j in [0]), name='shipments_constraints_retailer'
-    )
-
-    # - Other nodes
-    # model.addConstrs(
-    #     (_shipments[j, k] <= _backlogs[j, k - 1] + _orders[j - 1, k - L_info[j]]
-    #      for k in range(K) for j in range(1, J)), name='shipments_constraints_retailer'
-    # )
-
-    # shipments <-> backlog
-    # Other nodes
-    model.addConstrs(
-        (_shipments[j, k] - _orders[j - 1, k - L_info[j - 1]] == _backlogs[j, k - 1] - _backlogs[j, k]
+        (gp.quicksum([_shipments[j, t] for t in range(k + 1)]) ==
+         gp.quicksum([_orders[j - 1, t - L_info[j - 1]] for t in range(k + 1)]) - _backlogs[j, k] + _backlogs[j, -1]
          for j in range(1, J) for k in range(K))
     )
 
-    # Retailer
+    _x, _y = define_bin_variables(model, J, K)
+
     model.addConstrs(
-        (_shipments[j, k] - demands[k] == _backlogs[j, k - 1] - _backlogs[j, k]
-         for j in [0] for k in range(K))
+        (_inventory[j, k] <= _x[j, k] * 100000 for j in range(J) for k in range(K))
     )
 
-    # # shipments <-> inventory
-    # Other nodes
     model.addConstrs(
-        (_shipments[j + 1, k - L_ship[j]] - _shipments[j, k] == _inventory[j, k] - _inventory[j, k - 1]
-         for j in range(J - 1) for k in range(K))
+        (_backlogs[j, k] <= _y[j, k] * 100000 for j in range(J) for k in range(K))
     )
 
-    # Upmost supplier
     model.addConstrs(
-        (_orders[j, k - (L_ship[j] + L_info[j])] - _shipments[j, k] == _inventory[j, k] - _inventory[j, k - 1]
-         for j in [J - 1] for k in range(K))
+        (_x[j, k] + _y[j, k] <= 1 for j in range(J) for k in range(K))
     )
+
+    model.Params.LogToConsole = 0  # optimize in silence
+    #     model.setParam("MIPGap", 1e-10)
 
     model.setObjective(
         gp.quicksum([_inventory[j, k] * c_h[j] for j in range(J) for k in range(K)]) +
-        gp.quicksum([_backlogs[j, k] * c_b[j] for j in range(J) for k in range(K)]), GRB.MINIMIZE
+        gp.quicksum([_backlogs[j, k] * c_b[j] for j in range(J) for k in range(K)]) +
+        gp.quicksum(_orders[j, k] * 0.000001 for j in range(J) for k in range(K)),
+        GRB.MINIMIZE
     )
 
     model.optimize()
 
     return model, _orders, _inventory, _backlogs, _shipments
-
-demands = [8] * 100
-
-m, orders, inventory, backlogs, shipments = solve_by_gurobi(demands=demands)
-#
-plot_gurobi_variables(orders, name='orders')
-plot_gurobi_variables(inventory, name='inventory')
-plot_gurobi_variables(backlogs, name='backlogs')
-plot_gurobi_variables(shipments, name='shipments')
